fe3b1e86512d93e643aba8f76fe0f09f50710ba9
Abhiho11a <abhishekholla01@gmail.com>
2026-02-21 19:29:28 +0530

    Step-02 designed a user schema for database

---
+++ backend/model/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
+      index: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false, // Don't return password by default
+    },
+    timestamps:true
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
